084c161272987873d8533fbf9ae26e7ea6ddedfd
Ramon <Rahmon@users.noreply.github.com>
2019-02-20 10:12:32 -0300

    Refactoring to be things more simple =)

--- woo-for-logged-users.php
+++ woo-for-logged-users.php
@@ -23,21 +23,110 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-woo-for-logged-users.php';
+add_action( 'admin_menu', 'wflu_add_admin_menu' );
+add_action( 'admin_init', 'wflu_settings_init' );
 
-/**
- * Begins execution of the plugin.
- *
- * @since    1.0.0
- */
-function run_woo_for_logged_users() {
+function wflu_add_admin_menu(  ) { 
 
-	$plugin = new Woo_For_Logged_Users();
-	$plugin->run();
+	add_submenu_page( 'woocommerce', 'WooCommerce for logged users', 'WooCommerce for logged users', 'manage_options', 'woocommerce_for_logged_users', 'wflu_options_page' );
 
 }
-run_woo_for_logged_users();
+
+function wflu_settings_init(  ) { 
+
+	register_setting( 'wooForLoggedUsersPage', 'wflu_settings' );
+
+	add_settings_section(
+		'wflu_pluginPage_section', 
+		__( 'Settings', 'woo-for-logged-users' ), 
+		null, 
+		'wooForLoggedUsersPage'
+	);
+
+	add_settings_field( 
+		'wflu_checkbox_redirect_to_shop_after_login', 
+		__( 'Redirect to shop after login (default is to "My account" page)', 'woo-for-logged-users' ), 
+		'wflu_checkbox_redirect_to_shop_after_login_render', 
+		'wooForLoggedUsersPage', 
+		'wflu_pluginPage_section' 
+	);
+
+}
+
+function wflu_checkbox_redirect_to_shop_after_login_render(  ) { 
+
+	$options = get_option( 'wflu_settings' );
+	$checkbox_value = false;
+
+	if ( ! empty( $options ) ) {
+		$checkbox_value = $options['wflu_checkbox_redirect_to_shop_after_login'];
+	} 
+
+	?>
+	<input type='checkbox' name='wflu_settings[wflu_checkbox_redirect_to_shop_after_login]' <?php checked( $checkbox_value, 1 ); ?> value='1'>
+	<?php
+
+}
+
+function wflu_options_page(  ) { 
+
+	?>
+	<form action='options.php' method='post'>
+
+		<h2>WooCommerce for logged users</h2>
+
+		<?php
+		settings_fields( 'wooForLoggedUsersPage' );
+		do_settings_sections( 'wooForLoggedUsersPage' );
+		submit_button();
+		?>
+
+	</form>
+	<?php
+
+}
+
+/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	// function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'woo-for-logged-users',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	// }
+
+function redirect_not_logged_users() {
+	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
+		if ( ! is_user_logged_in() && ( is_woocommerce() || is_cart() || is_checkout() ) ) {
+			wp_safe_redirect( wc_get_page_permalink( 'myaccount' ) );
+			exit;
+		}
+	}
+}
+
+function redirect_after_login( $redirect_to ) {
+
+	$options = get_option( 'wflu_settings' );
+	$redirect_after_login = false;
+
+	if ( ! empty( $options ) ) {
+		$redirect_after_login = $options['wflu_checkbox_redirect_to_shop_after_login'];
+
+		if ( $redirect_after_login ) {
+			$shop_page = wc_get_page_permalink( 'shop' );
+			$redirect_to = $shop_page;
+		}
+	} 
+
+	return $redirect_to;
+}
+
+add_action( 'template_redirect', 'redirect_not_logged_users' );
+add_filter( 'woocommerce_login_redirect', 'redirect_after_login');
